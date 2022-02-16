b3f5684e19eb832b526afa20e11ef9a47832e32f
hitmaizer <jose.sousalves@gmail.com>
2022-02-16 17:34:48 +0000

    onclick filtering function

--- src/components/Author.jsx
+++ src/components/Author.jsx
@@ -5,7 +5,7 @@ import { ArrowRight } from '@styled-icons/bootstrap/ArrowRight'
 export default function Author(props) {
     return (
         <StyledAuthor>
-            <div className="author__btn flex-row" onClick={props.handleAuthor}>
+            <div className="author__btn flex-row" onClick={() => props.handleAuthor(props.currentQuote.quoteAuthor)}>
                 <div className="btn__text flex-col">
                     <h3 className="author__name">{props.currentQuote.quoteAuthor}</h3>
                     <p className="quote__genre">{props.currentQuote.quoteGenre}</p>
