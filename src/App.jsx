41fd9702667070b4f8e2795a45bd059eacf1fe4c
hitmaizer <jose.sousalves@gmail.com>
2022-02-16 17:34:36 +0000

    filtering all questions from same author

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import Author from './components/Author'
 import Quote from './components/Quote'
 import RandomBtn from './components/RandomBtn'
+import FilteredQuote from './components/FilteredQuote'
+import { nanoid } from 'nanoid'
 
 export default function App() {
     const [quotesData, setQuotesData] = React.useState([])
+    const [filteredQuotes, setFilteredQuotes] = React.useState([])
+    const [singleQuote, setSingleQuote] = React.useState(true)
     const [currentQuote, setCurrentQuote] = React.useState({
         quoteText: "Click the button to get a random quote",
         quoteAuthor: "Random Quote Generator",
@@ -19,23 +23,25 @@ export default function App() {
         
     }, [])
 
-    console.log(currentQuote)
-
     function getRandomQuote() {
         const randomQuote = Math.floor(Math.random() * quotesData.length)
         setCurrentQuote(quotesData[randomQuote])
     }
 
-    function handleAuthor() {
-
+    function handleAuthor(author) {
+        setFilteredQuotes(quotesData.filter(quote => quote.quoteAuthor === author))
+        setSingleQuote(!singleQuote)
     }
+
+    const quotesElements = filteredQuotes.map(quote => <FilteredQuote key={nanoid()} 
+        text={quote.quoteText} />)
 
     return (
         <div className="page__wrapper">
             <RandomBtn randomQuote={getRandomQuote} />
             <div className="quote__container flex-col">
-                <Quote data={quotesData}
-                currentQuote={currentQuote}  />
+                {singleQuote === false && filteredQuotes.length > 0 ? quotesElements : <Quote data={quotesData}
+                currentQuote={currentQuote}  />}
                 <Author data={quotesData}
                 currentQuote={currentQuote}
                 handleAuthor={handleAuthor} />
