d761ea6ab5fbfeee705466943369c4201a1628a0
hitmaizer <jose.sousalves@gmail.com>
2022-02-16 15:42:15 +0000

    style rules for quote component

---
+++ src/elements/StyledQuote.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export default styled.div`
+    
+    padding-left: 100px;
+    border-left: solid 8px #F7DF94;
+    font-family: 'Raleway', sans-serif;
+    font-weight: 500;
+    font-size: 36px;
+    line-height: 120%;
+    max-width: 614px;
+    display: inline-block;
+
+`
