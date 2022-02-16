8379d3effdb2c217dd3d5c0b8a6624e68f819f4f
hitmaizer <jose.sousalves@gmail.com>
2022-02-16 16:53:41 +0000

    props for handle author

--- src/components/Author.jsx
+++ src/components/Author.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import StyledAuthor from '../elements/StyledAuthor'
 import { ArrowRight } from '@styled-icons/bootstrap/ArrowRight'
 
-export default function Author() {
+export default function Author(props) {
     return (
         <StyledAuthor>
-            <div className="author__btn flex-row">
+            <div className="author__btn flex-row" onClick={props.handleAuthor}>
                 <div className="btn__text flex-col">
-                    <h3 className="author__name">Bill Gates</h3>
-                    <p className="quote__genre">business</p>
+                    <h3 className="author__name">{props.currentQuote.quoteAuthor}</h3>
+                    <p className="quote__genre">{props.currentQuote.quoteGenre}</p>
                 </div>
                     <ArrowRight size="32px" className="btn__icon"/>
             </div>
