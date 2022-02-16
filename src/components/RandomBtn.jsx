a302d4b5798881b495dde8a8b543560bda6ef8b8
hitmaizer <jose.sousalves@gmail.com>
2022-02-16 16:55:05 +0000

    onClick random quote

--- src/components/RandomBtn.jsx
+++ src/components/RandomBtn.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Refresh } from '@styled-icons/ionicons-solid/Refresh'
 
-export default function RandomBtn() {
+export default function RandomBtn(props) {
     return (
         <div className="random__quote flex-row">
-            <button className="random__btn flex-row">
+            <button className="random__btn flex-row" onClick={props.randomQuote}>
                 <span className="btn__text">Get random quote</span>
                 <Refresh size="24px" />
             </button>
