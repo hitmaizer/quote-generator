6feed62f5732cce4a66e480b415726a147c5bf40
hitmaizer <jose.sousalves@gmail.com>
2022-02-16 15:02:58 +0000

    fetched quotes data

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
 import React from 'react'
 
 export default function App() {
+    const [quotesData, setQuotesData] = React.useState([])
+
+    React.useEffect(() => {
+        fetch("https://quote-garden.herokuapp.com/api/v3/quotes?limit=1000")
+            .then(res => res.json())
+            .then(data => setQuotesData(data))
+    }, [])
+
+    console.log(quotesData)
+
     return (
         <h1>Hello mate!</h1>
     )
