78e08b2dbcafb735d0344fe0f23120c6652600e2
hitmaizer <jose.sousalves@gmail.com>
2022-02-16 15:41:54 +0000

    jsx markup for quote component

---
+++ src/components/Quote.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import StyledQuote from '../elements/StyledQuote'
+
+export default function Quote() {
+    return (
+        <StyledQuote>
+            <p className="quote__text">
+            “The first rule of any technology used in
+            a business is that automation applied to an 
+            efficient operation will magnify the efficiency. 
+            The second is that automation applied to an inefficient 
+            operation will magnify the inefficiency.”
+            </p>
+        </StyledQuote>
+    )
+}
