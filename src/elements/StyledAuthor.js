4e039fe07ab9418173be91f45fa89925b0693b3a
hitmaizer <jose.sousalves@gmail.com>
2022-02-16 16:03:06 +0000

    basic styling for author element

---
+++ src/elements/StyledAuthor.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export default styled.div`
+    cursor: pointer;
+    display: inline-block;
+    padding: 32px;
+    &:hover {
+        background-color: #333333;
+
+        .author__name {
+            color: #ffffff;
+        }
+    }
+` 
