5fccab969543c1e19cb74553ca146601d68f1da4
hitmaizer <jose.sousalves@gmail.com>
2022-02-16 17:57:05 +0000

    conditional render of number of results

--- src/components/Author.jsx
+++ src/components/Author.jsx
@@ -3,12 +3,16 @@ import StyledAuthor from '../elements/StyledAuthor'
 import { ArrowRight } from '@styled-icons/bootstrap/ArrowRight'
 
 export default function Author(props) {
+    
+    console.log(props.state)
+
     return (
         <StyledAuthor>
             <div className="author__btn flex-row" onClick={() => props.handleAuthor(props.currentQuote.quoteAuthor)}>
                 <div className="btn__text flex-col">
                     <h3 className="author__name">{props.currentQuote.quoteAuthor}</h3>
                     <p className="quote__genre">{props.currentQuote.quoteGenre}</p>
+                    {props.state === false && <p className="quote__genre">Found {props.filteredQuotes.length} results</p>}
                 </div>
                     <ArrowRight size="32px" className="btn__icon"/>
             </div>
