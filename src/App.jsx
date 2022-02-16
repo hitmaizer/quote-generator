53f74d0dab029a18f510367285ff1f59432b0905
hitmaizer <jose.sousalves@gmail.com>
2022-02-16 16:54:30 +0000

    initiate state default text + working random quote

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,40 @@ import RandomBtn from './components/RandomBtn'
 
 export default function App() {
     const [quotesData, setQuotesData] = React.useState([])
+    const [currentQuote, setCurrentQuote] = React.useState({
+        quoteText: "Click the button to get a random quote",
+        quoteAuthor: "Random Quote Generator",
+        quoteGenre: "randomizer"
+    })
 
     React.useEffect(() => {
         fetch("https://quote-garden.herokuapp.com/api/v3/quotes?limit=1000")
             .then(res => res.json())
-            .then(data => setQuotesData(data))
+            .then(data => setQuotesData(data.data))
+
+        
     }, [])
 
-    console.log(quotesData)
+    console.log(currentQuote)
+
+    function getRandomQuote() {
+        const randomQuote = Math.floor(Math.random() * quotesData.length)
+        setCurrentQuote(quotesData[randomQuote])
+    }
+
+    function handleAuthor() {
+
+    }
 
     return (
         <div className="page__wrapper">
-            <RandomBtn />
+            <RandomBtn randomQuote={getRandomQuote} />
             <div className="quote__container flex-col">
-                <Quote />
-                <Author />
+                <Quote data={quotesData}
+                currentQuote={currentQuote}  />
+                <Author data={quotesData}
+                currentQuote={currentQuote}
+                handleAuthor={handleAuthor} />
             </div>
         </div>
     )
